322d0b41e7f8f5dbd3c3ee25c69b58677c2c5521
Brian James <brianpja@gmail.com>
2018-02-06 12:16:32 -0800

    book list container

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
 
+import BookList from '../containers/book-list';
+
 export default class App extends Component {
   render() {
     return (
-      <div>Brian is smart</div>
+      <div>
+        <div>Brian is smart</div>
+        <BookList />
+      </div>
     );
   }
 }
---
+++ src/containers/book-list.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class BookList extends Component {
+  renderList() {
+    return this.props.books.map((book) => {
+      return (
+        <li key={book.title} className="list-group-item">{book.title}</li>
+      );
+    });
+  }
+
+  render() {
+    return (
+      <ul className="list-group col-sm-4">
+        {this.renderList()}
+      </ul>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  //what is returned will be props inside BookList
+  return {
+    books: state.books
+  };
+}
+
+//connect function glues this function to the component to create
+//a container <BookList />
+export default connect(mapStateToProps)(BookList);
